9935d6132d9247314711768a98d40dff6e412a47
JF <jfgeerinckx@mac.com>
2018-10-30 11:00:02 +0100

    delete welcome.js

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './Welcome';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          Hello
-          <Welcome parametre={'HelloWorld'} />
+          YO
       </div>
     );
   }
