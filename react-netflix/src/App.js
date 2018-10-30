f9c2c8f4edec697581b39c22687ba1cd536ef1ba
JF <jfgeerinckx@mac.com>
2018-10-30 14:46:52 +0100

    changement de place movies.json + ajout code App.js

--- react-netflix/src/App.js
+++ react-netflix/src/App.js
@@ -1,14 +1,56 @@
-import React, { Component } from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import data from './movies.json';
 
-class App extends Component {
-  render() {
+function AppRouter () {
+  return (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link class="home-button" to="/">Homepage</Link>
+          </li>
+          <li>
+            <Link class="home-button" to="/showCard/">showCard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Index} />
+      <Route path="/showCard/" component={showCard} />
+    </div>
+  </Router>
+);
+  }
+
+function Index() {
     return (
-      <div className="App">
-          YO
+      <div>
+        <h2>Homepage</h2>
       </div>
     );
   }
-}
 
-export default App;
+function showCard() {
+  return (
+    <div>
+      <div class="App-header">
+        <h2>showCard</h2>
+          <div class="card_container">
+          { 
+            data.movie.map(function(movie){
+            return <h4 class="main-title">{movie.Title}</h4>
+            
+            ;
+            })
+          }
+          </div>
+      </div>
+    </div>
+  );
+} 
+ 
+
+export default AppRouter;
